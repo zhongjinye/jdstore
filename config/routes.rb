fab32e8b2cfbe686d743077090b270ac373b7a61
zhongjinye <610505041@qq.com>
2021-04-01 14:08:59 +0800

    implement product show

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
+  root 'products#index'
   devise_for :users
 
   namespace :admin do
     resources :products
   end
 
-  root 'welcome#index'
+
+  resources :products
 end
